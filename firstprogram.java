28b9ecb8afdec081097fd29387306bec98ccc06c
prasadbylapudi <31813770+prasadbylapudi@users.noreply.github.com>
2019-07-09 23:06:14 +0530

    Add files via upload

---
+++ firstprogram.java
@@ -0,0 +1,22 @@
+ /*
+public class firstprogram
+{
+	public static void main(String args[])
+	{
+		System.out.println("hello world"); 
+	}
+}
+  
+*/
+public class firstprogram
+{
+	public static void main(String args[])
+	{
+	 int a=5;// we need to know about the declaration rules.wecan use $(dollar)symbol.
+	 a=9; // in java you can change values.
+	 System.out.println(a);
+	 //float f=10f;//for float u have to use the f at the end to know it is float.
+	 //double d=5.5
+	 
+	}
+}
