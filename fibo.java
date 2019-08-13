cbbbb73f5a07c784a87e98e99c75e1f1d47d144e
PrasadBylapudi <31813770+prasadbylapudi@users.noreply.github.com>
2019-08-13 16:56:18 +0530

    fibonacci

--- fibo.java
+++ fibo.java
@@ -1,4 +1,15 @@
-run:
-Enter the number to find the FACTORIAL : 5
-
-The FACTORIAL of 5 is : 1BUILD SUCCESSFUL (total time: 4 seconds)
+class FibonacciExample1{  
+public static void main(String args[])  
+{    
+ int n1=0,n2=1,n3,i,count=10;    
+ System.out.print(n1+" "+n2);//printing 0 and 1    
+    
+ for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}  
