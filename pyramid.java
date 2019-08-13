06842dd6878ca65daac3b2368c10f3f9c9b79f83
PrasadBylapudi <31813770+prasadbylapudi@users.noreply.github.com>
2019-08-13 16:46:40 +0530

    programs

---
+++ pyramid.java
@@ -0,0 +1,4 @@
+run:
+Enter the number to find the FACTORIAL : 5
+
+The FACTORIAL of 5 is : 1BUILD SUCCESSFUL (total time: 4 seconds)
---
+++ pyramid_upsidedown.java
@@ -0,0 +1,9 @@
+run:
+Enter the height of the pyramid : 6
+* * * * * * * * * * * 
+  * * * * * * * * * 
+    * * * * * * * 
+      * * * * * 
+        * * * 
+          * 
+BUILD SUCCESSFUL (total time: 2 seconds)
