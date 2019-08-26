9808f48f1d6e51623951f0a6c768498c36a46b32
PrasadBylapudi <31813770+prasadbylapudi@users.noreply.github.com>
2019-08-26 09:25:22 +0530

    Create exercise8.java

---
+++ exercise8.java
@@ -0,0 +1,48 @@
+class A
+{
+   void display1()
+    {
+        System.out.println("A's display1");
+    }
+    void display2()
+    {
+        System.out.println("A's display2");
+    }
+    void display3()
+    {
+        System.out.println("A's display 3");
+    }
+}
+
+class  B extends A
+{
+    void display1()
+    {
+        System.out.println("B 's display1");
+    }
+    void display4()
+    {
+        System.out.println("B 's display4 ");
+    }
+        
+    
+}
+class C extends B
+{
+     void display3()
+    {
+        System.out.println("c 's display 3");
+    }
+}
+public class Main
+{
+	public static void main(String[] args)
+	{
+	 A a=new A();
+	 a.display1();
+	 B b=new B();
+	 b.display1();
+	 B c=new C();
+	 c.display3();
+	}
+}
